196618e9a59eeb2ab16655056250df918207876b
JeetKhatri <jeet.g.khatri@gmail.com>
2019-06-05 19:18:52 +0530

    add no using post method

--- calc/views.py
+++ calc/views.py
@@ -8,7 +8,7 @@ def home(request):
     return render(request, 'home.html', {'name':'Jeet'})
 
 def add(request):
-    no1 = int(request.GET["no1"])
-    no2 = int(request.GET["no2"])
+    no1 = int(request.POST["no1"])
+    no2 = int(request.POST["no2"])
     res = no1+no2
     return render(request, 'result.html', {'result':res})
