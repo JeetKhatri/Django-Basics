bc22ec9af5b855dbb59383e5071132a3a6b94647
JeetKhatri <jeet.g.khatri@gmail.com>
2019-06-06 15:31:19 +0530

    Passing Dynamic Data2 - pass list, iterate it, image

--- travello/views.py
+++ travello/views.py
@@ -7,14 +7,19 @@ def index(request):
     dest1.name = 'Ahmedabad'
     dest1.desc = 'Ahmedabad description'
     dest1.price = 500
+    dest1.imagePath = 'destination_1.jpg'
 
     dest2 = Destination()
     dest2.name = 'Mumbai'
     dest2.desc = 'Mumbai description'
     dest2.price = 700
+    dest2.imagePath = 'destination_2.jpg'
 
     dest3 = Destination()
     dest3.name = 'Bangalore'
     dest3.desc = 'Bangalore description'
     dest3.price = 1000
-    return render(request, 'index.html', {'dest1' : dest1, 'dest2' : dest2, 'dest3' : dest3})
+    dest3.imagePath = 'destination_3.jpg'
+
+    dests = [dest1, dest2, dest3]
+    return render(request, 'index.html', {'dests' : dests})
