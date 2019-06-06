26990f93eebf2c344eeb6ea96a44615d6bd0a9f3
JeetKhatri <jeet.g.khatri@gmail.com>
2019-06-06 15:14:54 +0530

    Passing Dynamic Data - destination name, desc, price

--- travello/views.py
+++ travello/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+from travello.models import Destination
 # Create your views here.
+
 def index(request):
-    return render(request, 'index.html', {'name':'Jeet'})
+    dest1 = Destination()
+    dest1.name = 'Ahmedabad'
+    dest1.desc = 'Ahmedabad description'
+    dest1.price = 500
+
+    dest2 = Destination()
+    dest2.name = 'Mumbai'
+    dest2.desc = 'Mumbai description'
+    dest2.price = 700
+
+    dest3 = Destination()
+    dest3.name = 'Bangalore'
+    dest3.desc = 'Bangalore description'
+    dest3.price = 1000
+    return render(request, 'index.html', {'dest1' : dest1, 'dest2' : dest2, 'dest3' : dest3})
